f4f614372ce92f00c6c3440548007e5836b29450
Jin Wei <seetohjinwei@gmail.com>
2023-08-18 13:18:07 +0800

    add commented out nvim-neorg

--- init.lua
+++ init.lua
@@ -484,6 +484,30 @@ require('lazy').setup({
     end
   },
 
+  -- {
+  --   "nvim-neorg/neorg",
+  --   build = ":Neorg sync-parsers",
+  --   dependencies = { "nvim-lua/plenary.nvim" },
+  --   config = function()
+  --     require("neorg").setup {
+  --       load = {
+  --         ["core.defaults"] = {}, -- Loads default behaviour
+  --         ["core.concealer"] = {}, -- Adds pretty icons to your documents
+  --         ["core.dirman"] = { -- Manages Neorg workspaces
+  --           config = {
+  --             workspaces = {
+  --               nus = "~/norg/nus",
+  --               notes = "~/norg/notes",
+  --             },
+  --           },
+  --         },
+  --       },
+  --     }
+  --
+  --     vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, { pattern = {"*.norg"}, command = "set conceallevel=3" })
+  --   end,
+  -- },
+
   -- putting everything directly in this table instead
   -- { import = 'custom.plugins' },
 }, {})
